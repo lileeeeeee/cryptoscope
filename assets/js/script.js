8fcd974ffccf4569c849fd30f9ba09caa2341505
Will Burnton <willburnton@Wills-MacBook-Air.local>
2022-12-01 21:21:58 -0600

    api java test

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,14 +1,24 @@
-var queryHoroscope ='https://aztro.sameerkumar.website/?sign=aries&day=today';
+var queryHoroscope ='https://aztro.sameerkumar.website/?sign=aquarius&day=today';
 fetch(queryHoroscope, {
     method: 'POST'
 })
 .then(response => response.json())
 .then(json => {
-    const date = json.current_date;
-    console.log(date);
+    const luckyNumber = json.lucky_number;
+    console.log(luckyNumber);
+    let cryptoChoice = luckyNumber % 8; 
+    console.log(cryptoChoice);
+    localStorage.setItem("lucky", cryptoChoice); 
 });
 
-
-var queryCrypto = "https://api.coingecko.com/api/v3/ping"
+var arrayPosition = localStorage.getItem("lucky");
+var queryCrypto = "https://api.coingecko.com/api/v3/search/trending";
 fetch(queryCrypto) 
-.then(response => console.log(response.json()));
+.then(response => response.json())
+.then(json => { 
+    let coins = json.coins; 
+    console.log(coins);
+    console.log(coins[arrayPosition].item.id);
+
+}); 
+
