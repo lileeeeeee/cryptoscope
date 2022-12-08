68abb639e053e0466b97e47d3666c4aa4f85066e
wasad <wasadaliso@yahoo.com>
2022-12-07 20:41:03 -0600

    set up yesterday/tomorrow info

--- assets/js/APIdisplay.js
+++ assets/js/APIdisplay.js
@@ -1,5 +1,7 @@
 var sign = localStorage.getItem("sign");
-var queryHoroscope ='https://aztro.sameerkumar.website/?sign=' + sign + '&day=today'; 
+var queryHoroscope ='https://aztro.sameerkumar.website/?sign=' + sign + '&day=today';
+var queryHoroscopeYesterday ='https://aztro.sameerkumar.website/?sign=' + sign + '&day=yesterday';
+var queryHoroscopeTomorrow ='https://aztro.sameerkumar.website/?sign=' + sign + '&day=tomorrow'; 
 var cryptoNameEl = document.querySelector("#cryptoName"); 
 var priceEl = document.querySelector("#price"); 
 var rankEl = document.querySelector("#marketRank");
@@ -19,13 +21,60 @@ fetch(queryHoroscope, {
     var horoscopeDescription = document.querySelector('#horo-Description');
     var headline = document.querySelector('#headline');
     var horDate = document.querySelector('#date');
-    var luckynumba = document.querySelector('#luckynumba');
-    luckynumba.textContent = "Your Lucky Number is: " + luckyNumber;
+    var mood = document.querySelector('#mood');
+    mood.textContent = "Mood: " + json.mood;
     horDate.textContent = json.date_range;
     headline.textContent = "Welcome " + sign + "!";
     horoscopeDescription.textContent = json.description;
     console.log(sign);
 });
+
+fetch(queryHoroscopeYesterday, {
+    method: 'POST'
+})
+.then(response => response.json())
+.then(json => {
+    const luckyNumber = json.lucky_number;
+    console.log(luckyNumber);
+    let cryptoChoice = luckyNumber % 8; 
+    console.log(cryptoChoice);
+    localStorage.setItem("lucky", cryptoChoice); 
+    console.log(queryHoroscopeYesterday);
+    
+    var horoscopeDescriptionY = document.querySelector('#horo-DescriptionY');
+    var headlineY = document.querySelector('#headlineY');
+    var horDateY = document.querySelector('#dateY');
+    var moodY = document.querySelector('#moodY');
+    moodY.textContent = "Mood: " + json.mood;
+    horDateY.textContent = "Yesterday's Horoscope";
+    headlineY.textContent = "Welcome " + sign + "!";
+    horoscopeDescriptionY.textContent = json.description;
+    console.log(sign);
+});
+
+fetch(queryHoroscopeTomorrow, {
+    method: 'POST'
+})
+.then(response => response.json())
+.then(json => {
+    const luckyNumber = json.lucky_number;
+    console.log(luckyNumber);
+    let cryptoChoice = luckyNumber % 8; 
+    console.log(cryptoChoice);
+    localStorage.setItem("lucky", cryptoChoice); 
+    console.log(queryHoroscopeTomorrow);
+    
+    var horoscopeDescriptionT = document.querySelector('#horo-DescriptionT');
+    var headlineT = document.querySelector('#headlineT');
+    var horDateT = document.querySelector('#dateT');
+    var moodT = document.querySelector('#moodT');
+    moodT.textContent = "Mood: " + json.mood;
+    horDateT.textContent = "Tomorrow's Horoscope";
+    headlineT.textContent = "Welcome " + sign + "!";
+    horoscopeDescriptionT.textContent = json.description;
+    console.log(sign);
+});
+
 
 var arrayPosition = localStorage.getItem("lucky");
 var queryCrypto = "https://api.coingecko.com/api/v3/search/trending";
